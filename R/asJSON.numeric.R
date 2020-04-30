9f1693a69238ad30d1fc3dbc84206ba2c0d4e5a2
Frederik Proske <fproske@gams.com>
2020-04-30 17:48:00 +0200

    - removed unused argument from integer64_to_char function

--- R/asJSON.numeric.R
+++ R/asJSON.numeric.R
@@ -25,7 +25,7 @@ asjson_numeric_fun <- function(x, digits = 5, use_signif = is(digits, "AsIs"),
 
   # fast C implementation
   tmp <- if(is(x, "integer64")){
-    integer64_to_char(x, na_as_string, na_as_null)
+    integer64_to_char(x, na_as_string)
   } else {
     num_to_char(x, digits, na_as_string, use_signif, always_decimal, na_as_null);
   }
